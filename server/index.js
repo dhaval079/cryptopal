9d1ec2155a0c2692745b52fb7b43fbb1b2c0eafd
Meet Thakkar <meetprakash342004@gmail.com>
2024-01-27 11:59:40 +0530

    Initial Setup Done

---
+++ server/index.js
@@ -0,0 +1,20 @@
+const connectToMongo = require("./db")
+connectToMongo();
+const port = 5000
+const express = require('express')
+
+
+const app = express()
+var cors = require("cors")
+app.use(cors())
+app.use(express.json())
+
+
+// app.use("/api/user", require("./routes/user"));
+// app.use("/api/group", require("./routes/group"));
+// app.use("/api/expense", require("./routes/expense"));
+
+
+app.listen(port, ()=>{
+    console.log(`Server started succesfully at ${port}`);
+})
